006fbd7d7e4ec775394b423d92c49cb6bbe62b74
ira089 <gvir2015@gmail.com>
2024-06-23 00:02:47 +0300

    heder_start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './header.module.css';
 
+
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <NavLink className={styles.link} to="/">
-        Home
-      </NavLink>
-      <NavLink className={styles.link} to="/teachers">
-       Teachers
-      </NavLink>
-      <NavLink className={styles.link} to="/favorites">
-      Favorites
-      </NavLink>
-      
-    </div>
+    <section className={styles.header}>
+        <div className={styles.logo}>
+           <p className={styles.circle}></p>           
+           <p> LearnLingo</p>
+        </div>
+
+        <div className={styles.navigation}>
+           <NavLink className={styles.linkNavigation} to="/">Home</NavLink>
+           <NavLink className={styles.linkNavigation} to="/teachers">Teachers</NavLink>
+        </div>
+
+        <div className={styles.registration}>
+            <NavLink className={styles.linkRegistration}>Log in</NavLink>
+            <NavLink className={styles.linkRegistration}>Registration</NavLink>
+        </div>
+    </section>
+    
+    
   );
 };
 export default Header;
+
+
+
+    
+
