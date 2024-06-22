2c3220f2f5241063b9bd84ef70cbac83c7bc6c20
ira089 <gvir2015@gmail.com>
2024-06-22 15:02:30 +0300

    pageGreete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+// import { lazy } from 'react';
+import React from 'react';
+import HomePage from '../Pages/HomePage/HomePage';
+import  Layout  from '../Pages/Layout/Layout';
+import FavoritesPage from '../Pages/FavoritesPage/FavoritesPage';
+import TeachersPage from '../Pages/TeachersPage/TeachersPage';
+
+// const CatalogPage = lazy(() => import('../Pages/CatalogPage/CatalogPage'));
+// const FavoritesPage = lazy(() => import('../Pages/FavoritesPage/FavoritesPage'))
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/teachers" element={<TeachersPage />} >
+          </Route>
+          <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<HomePage />}/>
+      </Route>
+    </Routes>
   );
 };
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <NavLink className={styles.link} to="/">
+        Home
+      </NavLink>
+      <NavLink className={styles.link} to="/teachers">
+       Teachers
+      </NavLink>
+      <NavLink className={styles.link} to="/favorites">
+      Favorites
+      </NavLink>
+      
+    </div>
+  );
+};
+export default Header;
